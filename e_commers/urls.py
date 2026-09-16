fcc3d8bbc75b78a273ba388b898acaf8a9e83ddc
sooraj1312 <soorajxaviar0@gmail.com>
2026-09-16 11:10:08 +0530

    updated settings.py(added role based authorization) and updated model in user(created User)

--- e_commers/urls.py
+++ e_commers/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("users.urls")),
-    path('products/', include("products.urls")),
-    path('cart/', include("cart.urls")),
-    path('orders/', include("orders.urls")),
+    #path('users/', include("users.urls")),
+    #path('products/', include("products.urls")),
+    #path('cart/', include("cart.urls")),
+    #path('orders/', include("orders.urls")),
 ]
