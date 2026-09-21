40eff981e703688870b64cf4673e1b2a225c48e0
sooraj1312 <soorajxaviar0@gmail.com>
2026-09-21 09:29:02 +0530

    updated views and authentications

--- e_commers/settings.py
+++ e_commers/settings.py
@@ -69,7 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = { 
     'DEFAULT_AUTHENTICATION_CLASSES': ( 'rest_framework_simplejwt.authentication.JWTAuthentication', ),
-    'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.AllowAny', ), 
+    'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.IsAuthenticated', ), 
 }
 
 ROOT_URLCONF = 'e_commers.urls'
--- e_commers/urls.py
+++ e_commers/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('users/', include("users.urls")),
+    path('api/users/', include('users.urls')),
     #path('products/', include("products.urls")),
     #path('cart/', include("cart.urls")),
     #path('orders/', include("orders.urls")),
